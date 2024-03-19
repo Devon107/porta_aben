63d6cfa76d92f726de34765cac978fe9742e36f1
Devon107 <abiclank@gmail.com>
2024-03-19 12:58:40 -0600

    idiomas, cambios al color de los tags

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -21,7 +21,7 @@ export const experiences : experience[] = [
         finish:"Julio, 2021" ,
         company:"UAssistMe",
         descript:"Desarrollo de webapp para control de empleados 'AppUassistme' utilizando framework Laravel, también desarrolle sitios web con Wordpress y theme Divi.",
-        technologies:['php','wordpress', 'Divi', 'javascript', 'csharp', 'html5', 'css', 'bootstrap'],
+        technologies:['php','wordpress', 'Divi', 'javascript', 'csharp', 'html5', 'css', 'bootstrap','sqlite','mysql'],
 	},
 	{
         cargo:"Web Developer" ,
@@ -29,7 +29,7 @@ export const experiences : experience[] = [
         finish:"Julio, 2019" ,
         company:"Dive_ Digital",
         descript:"Desarrollo de landing pages utilizando Wordpress, Javascript, HTML, CSS, CronJobs en servidor apache.",
-        technologies:['php','wordpress', 'javascript', 'html5', 'css'],
+        technologies:['php','wordpress', 'javascript', 'html5', 'css','bootstrap','mysql'],
 	},
     {
         cargo:"Jr Developer",
@@ -37,6 +37,6 @@ export const experiences : experience[] = [
         finish:"Agosto, 2018",
         company:"Avalogics",
         descript:"Desarrollo de sitios web utilizando JavaScript, HTML, CSS, conociendo por primera vez el uso de Wordpress.",
-        technologies:['php','wordpress', 'Divi', 'javascript', 'html5', 'css'],
+        technologies:['php','wordpress', 'Divi', 'javascript', 'html5', 'css','sqlite','mysql'],
     }
 ] as const
--- src/constants/hero.ts
+++ src/constants/hero.ts
@@ -5,12 +5,14 @@ interface heroinfo {
     funcion: string
     technologies: string[]
     contacto: { text: string, link: string }[]
+    idioms: { text: string, level: string }[]
 }
 export const hero : heroinfo = {
         name:"Abinadi Henríquez",
 		image:"/098A3B61-B83D-4CED-804E-A5B738D86AE3_1_105_c.jpeg",
 		cargo:"Desarrollador Web",
 		funcion:"Full Stack, Apps de escritorio",
-		technologies:['php','csharp','wordpress', 'Divi', 'react', 'astro', 'javascript','typescript','html5', 'css', 'bootstrap', 'tailwindcss'],
+		technologies:['php','csharp','wordpress', 'Divi', 'react', 'astro', 'javascript','typescript','html5', 'css', 'bootstrap', 'tailwindcss','sqlite','mysql'],
 		contacto:[ { text: 'Linkedin', link: 'https://www.linkedin.com/in/abinadi-henriquez/' }, { text: 'Github', link: 'https://github.com/Devon107' }, {text: 'elmerg.uam@gmail.com', link: 'mailto:elmerg.uam@gmail.com'} ],
+        idioms:[{ text: 'Español', level: 'Nativo' }, { text: 'Inglés', level: 'Intermedio' }]
 } as const
