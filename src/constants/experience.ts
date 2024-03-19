10b13f79cf870ee3acbc89b522109d437aaa6cb3
Devon107 <abiclank@gmail.com>
2024-03-19 10:07:34 -0600

    cambios de nombre technologies

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -13,7 +13,7 @@ export const experiences : experience[] = [
 		finish:"" ,
 		company:"Vertikal",
 		descript:"Actualmente desarrollo sitios web y landing pages empleando Wordpress y theme Divi.",
-		technologies:['php_dark','wordpress', 'DIVI 1', 'javascript','html5', 'css'],
+		technologies:['php','wordpress', 'Divi', 'javascript','html5', 'css'],
 	},
 	{
 		cargo:"Web Developer" ,
@@ -21,7 +21,7 @@ export const experiences : experience[] = [
         finish:"Julio, 2021" ,
         company:"UAssistMe",
         descript:"Desarrollo de webapp para control de empleados 'AppUassistme' utilizando framework Laravel, también desarrolle sitios web con Wordpress y theme Divi.",
-        technologies:['php_dark','wordpress', 'DIVI 1', 'javascript', 'csharp', 'html5', 'css', 'bootstrap'],
+        technologies:['php','wordpress', 'Divi', 'javascript', 'csharp', 'html5', 'css', 'bootstrap'],
 	},
 	{
         cargo:"Web Developer" ,
@@ -29,7 +29,7 @@ export const experiences : experience[] = [
         finish:"Julio, 2019" ,
         company:"Dive_ Digital",
         descript:"Desarrollo de landing pages utilizando Wordpress, Javascript, HTML, CSS, CronJobs en servidor apache.",
-        technologies:['php_dark','wordpress', 'javascript', 'html5', 'css'],
+        technologies:['php','wordpress', 'javascript', 'html5', 'css'],
 	},
     {
         cargo:"Jr Developer",
@@ -37,6 +37,6 @@ export const experiences : experience[] = [
         finish:"Agosto, 2018",
         company:"Avalogics",
         descript:"Desarrollo de sitios web utilizando JavaScript, HTML, CSS, conociendo por primera vez el uso de Wordpress.",
-        technologies:['php_dark','wordpress', 'DIVI 1', 'javascript', 'html5', 'css'],
+        technologies:['php','wordpress', 'Divi', 'javascript', 'html5', 'css'],
     }
 ] as const
--- src/constants/hero.ts
+++ src/constants/hero.ts
@@ -11,6 +11,6 @@ export const hero : heroinfo = {
 		image:"/098A3B61-B83D-4CED-804E-A5B738D86AE3_1_105_c.jpeg",
 		cargo:"Desarrollador Web",
 		funcion:"Full Stack, Apps de escritorio",
-		technologies:['/php_dark.svg','/csharp.svg','/wordpress.svg', '/DIVI 1.svg', '/react.svg', '/astro_dark.svg', '/javascript.svg','/typescript.svg','/html5.svg', '/css.svg', '/bootstrap.svg', '/tailwindcss.svg'],
+		technologies:['php','csharp','wordpress', 'Divi', 'react', 'astro', 'javascript','typescript','html5', 'css', 'bootstrap', 'tailwindcss'],
 		contacto:[ { text: 'Linkedin', link: 'https://www.linkedin.com/in/abinadi-henriquez/' }, { text: 'Github', link: 'https://github.com/Devon107' }, {text: 'elmerg.uam@gmail.com', link: 'mailto:elmerg.uam@gmail.com'} ],
 } as const
