4a4a78d1d55237750509ff0f24a00e2f7aa3e689
Devon107 <abiclank@gmail.com>
2024-03-19 15:01:23 -0600

    change text about me

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -1,3 +1,2 @@
 export const about = "Hola 👋 mi nombre es Abinadi, soy ingeniero en Ciencias de la computación,"+
-                    " con más de 5 años de experiencia en la creación de aplicaciones web."+
-                    " Especializado en JavaScript, HTML, CSS, Laravel, Astro, React y Wordpress.";
+                    " con más de 5 años de experiencia en la creación de aplicaciones web.";
