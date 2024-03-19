c5ed114a6c9192a2358118479fb7f9c15e4b9183
Devon107 <abiclank@gmail.com>
2024-03-19 14:37:49 -0600

    added new projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,27 +4,31 @@ interface projects_content {
 	image: string
 	description: string
 	url:string
+	skills: string[]
 }
 export const projects : projects_content[] = [
 	{
 		id:"1",
-		title:"ACM Prado",
+		title:"ACM Prado - Catálogo de productos en linea",
 		image:"/acmprado/hero-acm-prado.webp",
-		description:"Catalogo de productos para la distribuidora ACM Prado",
-		url:"/proyectos/acm-prado",
+		description:"Este sitio web constituye un landing page con un catálogo de productos para la distribuidora ACM Prado y una página con información acerca de la empresa.",
+		url:"https://acmprado.com/",
+		skills:['php','wordpress','Divi','javascript','css']
 	},
 	{
 		id:"2",
-		title:"ACM Prado",
-		image:"/acmprado/hero-acm-prado.webp",
-		description:"Catalogo de productos para la distribuidora ACM Prado",
-		url:"/proyectos/acm-prado",
+		title:"Vertikal - Desarrollo de paginas web y tienda en línea",
+		image:"/vertikal/vertikal-home.webp",
+		description:"Sitio web informativo para la empresa desarrolladora Vertikal",
+		url:"https://vertikal.com.sv/",
+		skills:['wordpress','Divi','javascript','css']
 	},
 	{
 		id:"3",
-		title:"ACM Prado",
-		image:"/acmprado/hero-acm-prado.webp",
-		description:"Catalogo de productos para la distribuidora ACM Prado",
-		url:"/proyectos/acm-prado",
+		title:"MF Consultores - Consultores de Negocios",
+		image:"/mariof/mariof-home.webp",
+		description:"Sitio informativo sobre servicios de consultoria para la empresa MF-Consultores",
+		url:"https://mariofconsultores.com/",
+		skills:['php','wordpress','Divi','javascript','css']
 	}
 ] as const
