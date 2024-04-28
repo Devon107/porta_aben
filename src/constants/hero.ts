ff5973a778f202c589cdf3e836e28cc1ddf49777
Devon107 <abiclank@gmail.com>
2024-04-27 18:28:41 -0600

    Spelling changes

--- src/constants/hero.ts
+++ src/constants/hero.ts
@@ -11,7 +11,7 @@ export const hero : heroinfo = {
         name:"Abinadi Henríquez",
 		image:"/400049335_10228214801187124_5648549841730663410_n.webp",
 		cargo:"Desarrollador Web",
-		funcion:"Full Stack, Apps de escritorio",
+		funcion:"Full Stack",
 		technologies:['php','csharp','wordpress', 'Divi', 'react', 'astro', 'javascript','typescript','html5', 'css', 'bootstrap', 'tailwindcss','sqlite','mysql'],
 		contacto:[ { text: 'Linkedin', link: 'https://www.linkedin.com/in/abinadi-henriquez/' }, { text: 'Github', link: 'https://github.com/Devon107' }, {text: 'elmerg.uam@gmail.com', link: 'mailto:elmerg.uam@gmail.com'} ],
         idioms:[{ text: 'Español', level: 'Nativo' }, { text: 'Inglés', level: 'Intermedio' }]
