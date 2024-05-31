5063d3141e96bf2963c576f78f832308a002f6da
Devon107 <abiclank@gmail.com>
2024-05-30 19:42:45 -0600

    New experience added

--- src/constants/experience-en.ts
+++ src/constants/experience-en.ts
@@ -1,42 +1,102 @@
 interface experience {
-	cargo: string
-	initial: string
-	finish: string
-	company: string
-	descript:string
-    technologies:string[]
+  cargo: string;
+  initial: string;
+  finish: string;
+  company: string;
+  descript: string;
+  technologies: string[];
 }
-export const experiences : experience[] = [
-	{
-		cargo:"Web Developer",
-		initial:"October,2021", 
-		finish:"" ,
-		company:"Vertikal Company",
-		descript:"I currently develop static websites, e-commerce and landing pages.",
-		technologies:['php','wordpress', 'Divi', 'javascript','html5', 'css'],
-	},
-	{
-		cargo:"Web Developer" ,
-        initial:"October, 2019" ,
-        finish:"July, 2021" ,
-        company:"U Assist Me",
-        descript:"Development of software for employee control 'App Uassistme', also developing static websites and e-commerce.",
-        technologies:['php', 'laravel','wordpress', 'Divi', 'javascript', 'csharp', 'html5', 'css', 'bootstrap','sqlite','mysql'],
-	},
-	{
-        cargo:"Web Developer" ,
-        initial:"October, 2018" ,
-        finish:"July, 2019" ,
-        company:"Dive_ Digital",
-        descript:"Development of static websites, web applications and landing pages.",
-        technologies:['php','wordpress', 'javascript', 'html5', 'css','bootstrap','mysql'],
-	},
-    {
-        cargo:"Jr Developer",
-        initial:"March, 2018",
-        finish:"August, 2018",
-        company:"Avalogics",
-        descript:"Development of static websites, learning the use of Wordpress for the first time.",
-        technologies:['php','wordpress', 'Divi', 'javascript', 'html5', 'css','sqlite','mysql'],
-    }
-] as const
+export const experiences: experience[] = [
+  {
+    cargo: "Web Developer",
+    initial: "June, 2024",
+    finish: "",
+    company: "LeadX Marketing",
+    descript:
+      "Development of statistical websites, e-commerce, web applications and landing pages. Management of Google tag manager and Google analytics.",
+    technologies: [
+      "php",
+      "wordpress",
+      "shopify",
+      "webflow",
+      "Divi",
+      "javascript",
+      "html5",
+      "css",
+      "laravel",
+    ],
+  },
+  {
+    cargo: "Web Developer",
+    initial: "October,2021",
+    finish: "June, 2024",
+    company: "Vertikal Company",
+    descript:
+      "Development of statistical websites, e-commerce and landing pages.",
+    technologies: [
+      "php",
+      "wordpress",
+      "Divi",
+      "javascript",
+      "html5",
+      "css",
+      "laravel",
+    ],
+  },
+  {
+    cargo: "Web Developer",
+    initial: "October, 2019",
+    finish: "July, 2021",
+    company: "U Assist Me",
+    descript:
+      "Development of software for employee control 'App Uassistme', also developing statistical websites and e-commerce.",
+    technologies: [
+      "php",
+      "laravel",
+      "wordpress",
+      "Divi",
+      "javascript",
+      "csharp",
+      "html5",
+      "css",
+      "bootstrap",
+      "sqlite",
+      "mysql",
+    ],
+  },
+  {
+    cargo: "Web Developer",
+    initial: "October, 2018",
+    finish: "July, 2019",
+    company: "Dive_ Digital",
+    descript:
+      "Development of statistical websites, web applications and landing pages.",
+    technologies: [
+      "php",
+      "wordpress",
+      "javascript",
+      "html5",
+      "css",
+      "bootstrap",
+      "mysql",
+    ],
+  },
+  {
+    cargo: "Jr Developer",
+    initial: "March, 2018",
+    finish: "August, 2018",
+    company: "Avalogics",
+    descript:
+      "Development of statistical websites, learning the use of Wordpress for the first time.",
+    technologies: [
+      "php",
+      "wordpress",
+      "Divi",
+      "javascript",
+      "html5",
+      "css",
+      "sqlite",
+      "mysql",
+    ],
+  },
+] as const;
--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -1,42 +1,101 @@
 interface experience {
-	cargo: string
-	initial: string
-	finish: string
-	company: string
-	descript:string
-    technologies:string[]
+  cargo: string;
+  initial: string;
+  finish: string;
+  company: string;
+  descript: string;
+  technologies: string[];
 }
-export const experiences : experience[] = [
-	{
-		cargo:"Web Developer",
-		initial:"Octubre,2021", 
-		finish:"" ,
-		company:"Vertikal",
-		descript:"Actualmente desarrollo sitios web estáticos, e-commerce y landing pages.",
-		technologies:['php','wordpress', 'Divi', 'javascript','html5', 'css'],
-	},
-	{
-		cargo:"Web Developer" ,
-        initial:"Octubre, 2019" ,
-        finish:"Julio, 2021" ,
-        company:"UAssistMe",
-        descript:"Desarrollo de software para control de empleados 'AppUassistme', también desarrolle sitios web estáticos y e-commerce.",
-        technologies:['php', 'laravel','wordpress', 'Divi', 'javascript', 'csharp', 'html5', 'css', 'bootstrap','sqlite','mysql'],
-	},
-	{
-        cargo:"Web Developer" ,
-        initial:"Octubre, 2018" ,
-        finish:"Julio, 2019" ,
-        company:"Dive_ Digital",
-        descript:"Desarrolle sitios web estáticos, aplicativos web y landing pages.",
-        technologies:['php','wordpress', 'javascript', 'html5', 'css','bootstrap','mysql'],
-	},
-    {
-        cargo:"Jr Developer",
-        initial:"Marzo, 2018",
-        finish:"Agosto, 2018",
-        company:"Avalogics",
-        descript:"Desarrollo de sitios web estáticos, conociendo por primera vez el uso de Wordpress.",
-        technologies:['php','wordpress', 'Divi', 'javascript', 'html5', 'css','sqlite','mysql'],
-    }
-] as const
+export const experiences: experience[] = [
+  {
+    cargo: "Web Developer",
+    initial: "June, 2024",
+    finish: "",
+    company: "LeadX Marketing",
+    descript:
+      "Desarrollo de sitios web estaticos, e-commerce, Aplicativos web y landing pages. Manejo de Google tag manager y Google analitycs.",
+    technologies: [
+      "php",
+      "wordpress",
+      "shopify",
+      "webflow",
+      "Divi",
+      "javascript",
+      "html5",
+      "css",
+      "laravel",
+    ],
+  },
+  {
+    cargo: "Web Developer",
+    initial: "Octubre,2021",
+    finish: "Junio, 2024",
+    company: "Vertikal",
+    descript: "Desarrollo sitios web estáticos, e-commerce y landing pages.",
+    technologies: [
+      "php",
+      "wordpress",
+      "Divi",
+      "javascript",
+      "html5",
+      "css",
+      "laravel",
+    ],
+  },
+  {
+    cargo: "Web Developer",
+    initial: "Octubre, 2019",
+    finish: "Julio, 2021",
+    company: "UAssistMe",
+    descript:
+      "Desarrollo de software para control de empleados 'AppUassistme', también desarrolle sitios web estáticos y e-commerce.",
+    technologies: [
+      "php",
+      "laravel",
+      "wordpress",
+      "Divi",
+      "javascript",
+      "csharp",
+      "html5",
+      "css",
+      "bootstrap",
+      "sqlite",
+      "mysql",
+    ],
+  },
+  {
+    cargo: "Web Developer",
+    initial: "Octubre, 2018",
+    finish: "Julio, 2019",
+    company: "Dive_ Digital",
+    descript:
+      "Desarrolle sitios web estáticos, aplicativos web y landing pages.",
+    technologies: [
+      "php",
+      "wordpress",
+      "javascript",
+      "html5",
+      "css",
+      "bootstrap",
+      "mysql",
+    ],
+  },
+  {
+    cargo: "Jr Developer",
+    initial: "Marzo, 2018",
+    finish: "Agosto, 2018",
+    company: "Avalogics",
+    descript:
+      "Desarrollo de sitios web estáticos, conociendo por primera vez el uso de Wordpress.",
+    technologies: [
+      "php",
+      "wordpress",
+      "Divi",
+      "javascript",
+      "html5",
+      "css",
+      "sqlite",
+      "mysql",
+    ],
+  },
+] as const;
