d57f02bddd5b99cc4dcfdfa3b483a9e15fbd9ede
Devon107 <abiclank@gmail.com>
2024-05-30 19:42:29 -0600

    Matices project added

--- src/constants/projects-en.ts
+++ src/constants/projects-en.ts
@@ -1,34 +1,44 @@
 interface projects_content {
-	id: string
-	title: string
-	image: string
-	description: string
-	url:string
-	skills: string[]
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  url: string;
+  skills: string[];
 }
-export const projects : projects_content[] = [
-	{
-		id:"1",
-		title:"ACM Prado - Online product catalog",
-		image:"/acmprado/hero-acm-prado.webp",
-		description:"This website constitutes a landing page with a product catalog for the ACM Prado distributor and a page with information about the company.",
-		url:"https://acmprado.com/",
-		skills:['php','wordpress','Divi','javascript','css']
-	},
-	{
-		id:"2",
-		title:"Vertikal - Development of web pages and online store",
-		image:"/vertikal/vertikal-home.webp",
-		description:"Informative website for the development company Vertikal.",
-		url:"https://vertikal.com.sv/",
-		skills:['wordpress','Divi','javascript','css']
-	},
-	{
-		id:"3",
-		title:"MF Consultants - Business Consultants",
-		image:"/mariof/mariof-home.webp",
-		description:"Informative site on consulting services for the company MF-Consultores.",
-		url:"https://mariofconsultores.com/",
-		skills:['php','wordpress','Divi','javascript','css']
-	}
-] as const
+export const projects: projects_content[] = [
+  {
+    id: "1",
+    title: "Matrices by ruth",
+    image: "/matices/homepage.png",
+    description: "Ecommerce for hair care",
+    url: "https://maticesbyruth.com/",
+    skills: ["php", "wordpress", "Divi", "javascript", "css"],
+  },
+  {
+    id: "2",
+    title: "ACM Prado - Online product catalog",
+    image: "/acmprado/hero-acm-prado.webp",
+    description:
+      "This website constitutes a landing page with a product catalog for the ACM Prado distributor and a page with information about the company.",
+    url: "https://acmprado.com/",
+    skills: ["php", "wordpress", "Divi", "javascript", "css"],
+  },
+  {
+    id: "3",
+    title: "Vertikal - Development of web pages and online store",
+    image: "/vertikal/vertikal-home.webp",
+    description: "Informative website for the development company Vertikal.",
+    url: "https://vertikal.com.sv/",
+    skills: ["wordpress", "Divi", "javascript", "css"],
+  },
+  {
+    id: "4",
+    title: "MF Consultants - Business Consultants",
+    image: "/mariof/mariof-home.webp",
+    description:
+      "Informative site on consulting services for the company MF-Consultores.",
+    url: "https://mariofconsultores.com/",
+    skills: ["php", "wordpress", "Divi", "javascript", "css"],
+  },
+] as const;
--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,34 +1,45 @@
 interface projects_content {
-	id: string
-	title: string
-	image: string
-	description: string
-	url:string
-	skills: string[]
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  url: string;
+  skills: string[];
 }
-export const projects : projects_content[] = [
-	{
-		id:"1",
-		title:"ACM Prado - Catálogo de productos en linea",
-		image:"/acmprado/hero-acm-prado.webp",
-		description:"Este sitio web constituye un landing page con un catálogo de productos para la distribuidora ACM Prado y una página con información acerca de la empresa.",
-		url:"https://acmprado.com/",
-		skills:['php','wordpress','Divi','javascript','css']
-	},
-	{
-		id:"2",
-		title:"Vertikal - Desarrollo de paginas web y tienda en línea",
-		image:"/vertikal/vertikal-home.webp",
-		description:"Sitio web informativo para la empresa desarrolladora Vertikal",
-		url:"https://vertikal.com.sv/",
-		skills:['wordpress','Divi','javascript','css']
-	},
-	{
-		id:"3",
-		title:"MF Consultores - Consultores de Negocios",
-		image:"/mariof/mariof-home.webp",
-		description:"Sitio informativo sobre servicios de consultoria para la empresa MF-Consultores",
-		url:"https://mariofconsultores.com/",
-		skills:['php','wordpress','Divi','javascript','css']
-	}
-] as const
+export const projects: projects_content[] = [
+  {
+    id: "1",
+    title: "Matrices by ruth",
+    image: "/matices/homepage.png",
+    description: "Ecommerce para el cuidado del cabello",
+    url: "https://maticesbyruth.com/",
+    skills: ["php", "wordpress", "Divi", "javascript", "css"],
+  },
+  {
+    id: "2",
+    title: "ACM Prado - Catálogo de productos en linea",
+    image: "/acmprado/hero-acm-prado.webp",
+    description:
+      "Este sitio web constituye un landing page con un catálogo de productos para la distribuidora ACM Prado y una página con información acerca de la empresa.",
+    url: "https://acmprado.com/",
+    skills: ["php", "wordpress", "Divi", "javascript", "css"],
+  },
+  {
+    id: "3",
+    title: "Vertikal - Desarrollo de paginas web y tienda en línea",
+    image: "/vertikal/vertikal-home.webp",
+    description:
+      "Sitio web informativo para la empresa desarrolladora Vertikal",
+    url: "https://vertikal.com.sv/",
+    skills: ["wordpress", "Divi", "javascript", "css"],
+  },
+  {
+    id: "4",
+    title: "MF Consultores - Consultores de Negocios",
+    image: "/mariof/mariof-home.webp",
+    description:
+      "Sitio informativo sobre servicios de consultoria para la empresa MF-Consultores",
+    url: "https://mariofconsultores.com/",
+    skills: ["php", "wordpress", "Divi", "javascript", "css"],
+  },
+] as const;
